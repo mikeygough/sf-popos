e8ae25de6f75c5f75133eabd7c6285b0b2a0d9f2
mikey <mikey@hey.com>
2024-02-10 13:52:42 -0600

    add json for dynamic content

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -1,67 +1,23 @@
 import React from 'react';
+import data from './sfpopos-data.json';
 import './POPOSList.css';
 import POPOSSpace from './POPOSSpace';
 
 function POPOSList() {
-  return (
-    <div className="POPOSList">
+  // deconstruct as a parameter
+  const spaces = data.map(({ title, address, images, hours }) => {
+    return (
       <POPOSSpace
-        name="50 California Street"
-        address="50 California St."
-        image="50-california-st.jpg"
+        key={title}
+        name={title}
+        address={address}
+        image={images[0]}
+        hours={hours}
       />
-      <POPOSSpace
-        name="100 Pine Street"
-        address="100 Pine St."
-        image="100-pine.jpg"
-      />
-      <POPOSSpace
-        name="101 California Street"
-        address="101 California St."
-        image="101-california.jpg"
-      />
-      <POPOSSpace
-        name="343 Sansome Roof Garden"
-        address="343 Sansome St."
-        image="343-sansome-roof-garden.jpg"
-      />
-      <POPOSSpace
-        name="525 Market Street Plaza"
-        address="525 Market St."
-        image="525-market-street-plaza.jpg"
-      />
-      <POPOSSpace
-        name="555 California Street"
-        address="555 California St."
-        image="555-california.jpg"
-      />
-      <POPOSSpace
-        name="Citigroup Center"
-        address="1 Sansome St."
-        image="citigroup-center.jpg"
-      />
-      <POPOSSpace
-        name="Embarcadero Center"
-        address="2 Embarcadero Ctr."
-        image="embarcadero-center.jpg"
-      />
-      <POPOSSpace
-        name="Empire Park"
-        address="642 Commercial St."
-        image="empire-park.jpg"
-      />
-      <POPOSSpace
-        name="Garden Terrace at 150 California Street"
-        address="150 California St."
-        image="garden-terrace-at-150-california.jpg"
-      />
-      <POPOSSpace
-        name="Transamerica Redwood Park"
-        address="600 Montgomery St."
-        image="transamerica-redwood-park.jpg"
-      />
-    </div>
-  );
+    );
+  });
+
+  return <div className="POPOSList">{spaces}</div>;
 }
 
 export default POPOSList;
